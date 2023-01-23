dbb7f8c0426ed7bb2720912edd66728aeb5d0a04
silviagstnn <silviagustina1010@gmail.com>
2023-01-23 13:56:58 +0700

    login

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
 import Register from './pages/register_page';
+import Login from './pages/login_page';
 
 function App() {
   return (
    <>
-   <Register/>
+   <Login/>
    </>
   );
 }
